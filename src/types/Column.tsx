225ae6877a4ad5ab997baf4e550b587574b020c4
saharso <saharso@gmail.com>
2023-11-26 11:11:28 +0200

    new column type

---
+++ src/types/Column.tsx
@@ -0,0 +1,7 @@
+export default interface Column {
+  id: string;
+  ordinalNo: number;
+  title: string;
+  type: string;
+  width?: number;
+}
---
+++ src/types/index.ts
@@ -0,0 +1,1 @@
+export type { default as Column } from "./Column";
