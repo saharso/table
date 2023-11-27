38d0194fcbf8dbf9efe7acaddf5629cf90410b02
saharso <saharso@gmail.com>
2023-11-27 07:34:18 +0200

    add options field to Column type

--- src/Components/Table/types/Column.tsx
+++ src/Components/Table/types/Column.tsx
@@ -1,7 +1,16 @@
-export default interface Column<Row = unknown> {
+interface DefaultColumn<Row = unknown> {
   id: keyof Row;
   ordinalNo: number;
   title: string;
-  type: string;
+  type: "string" | "number" | "boolean" | "options";
   width?: number;
 }
+
+interface OptionsColumn<Row = unknown> extends DefaultColumn<Row> {
+  type: "options";
+  options: string[];
+}
+
+type Column<Row = unknown> = DefaultColumn<Row> | OptionsColumn<Row>;
+
+export default Column;
