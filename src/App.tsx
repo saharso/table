aad34cb1f76d89733674cdda0e09745e009655c1
saharso <saharso@gmail.com>
2023-11-27 08:32:16 +0200

    install material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import React, { useEffect } from "react";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import "./App.scss";
 // import generateMockEntry from "./utils/createMocks";
 import mock1 from "./mocks/mock1.json";
