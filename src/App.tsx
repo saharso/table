a7e675827f3c4701e8eb11771330003250ffb497
saharso <saharso@gmail.com>
2023-11-28 08:37:48 +0200

    move font imports to index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,6 @@ import React, {
   useEffect,
   useState,
 } from "react";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 import "./App.scss";
 import mock from "./mocks/mock.json";
 import { Table, ToolBar } from "./Components";
