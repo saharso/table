df2ba160d888cafce81ec2fbc33c9cbbbf6750d9
saharso <saharso@gmail.com>
2023-11-28 08:00:24 +0200

    add debounce to search
    add debounce to search

--- src/Components/ToolBar/ToolBar.tsx
+++ src/Components/ToolBar/ToolBar.tsx
@@ -2,12 +2,21 @@ import { TextField } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import React from "react";
 import styles from "./ToolBar.module.scss";
+import { debounce } from "lodash";
 
 interface ToolBarProps {
   onSearch: (value: string) => void;
   searchValue: string;
 }
 export default function ToolBar({ onSearch, searchValue }: ToolBarProps) {
+  const [displayValue, setDisplayValue] = React.useState<string>("");
+  const debouncedInputChange = debounce((value: string) => {
+    onSearch(value);
+  }, 100);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    debouncedInputChange(e.target.value);
+    setDisplayValue(e.target.value);
+  };
   return (
     <div className={styles.ToolBar}>
       <div className={"layout-align-y gap-2"}>
@@ -19,8 +28,8 @@ export default function ToolBar({ onSearch, searchValue }: ToolBarProps) {
           InputProps={{
             endAdornment: <SearchIcon />,
           }}
-          value={searchValue}
-          onChange={(e) => onSearch(e.target.value)}
+          value={displayValue}
+          onChange={handleChange}
         />
       </div>
     </div>
