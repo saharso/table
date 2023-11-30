5c6c05f52ba1004fd4671722f8ff40eb9d84dc71
saharso <saharso@gmail.com>
2023-11-30 20:12:12 +0200

    add testes

--- src/EnhancedTable/EnhancedTable.test.tsx
+++ src/EnhancedTable/EnhancedTable.test.tsx
@@ -1,9 +1,11 @@
 // TableComponent.test.jsx
 
 import React from "react";
-import { act, render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import EnhancedTable from "./EnhancedTable";
 import { Column } from "./types";
+import { VirtuosoMockContext } from "react-virtuoso";
+import { wait } from "@testing-library/user-event/dist/utils";
 
 describe("TableComponent", () => {
   it("should render table rows with correct data", async () => {
@@ -61,5 +63,36 @@ describe("TableComponent", () => {
         expect(rowContainer).toBeInTheDocument();
       })();
     });
+  });
+  it("should filter the table by search", async () => {
+    // Arrange
+    const data = [
+      { name: "John Doe", age: 25, id: "1" },
+      { name: "Jane Doe", age: 30, id: "2" },
+    ];
+    const columns: Column[] = [
+      { id: "name", title: "Name", type: "string" },
+      { id: "age", title: "Age", type: "number" },
+    ];
+    // Act
+    render(<EnhancedTable data={data} columns={columns} identifier={"id"} />, {
+      wrapper: ({ children }) => (
+        <VirtuosoMockContext.Provider
+          value={{ viewportHeight: 300, itemHeight: 100 }}
+        >
+          {children}
+        </VirtuosoMockContext.Provider>
+      ),
+    });
+
+    // Assert
+    const searchInput = screen.getByLabelText("Search");
+    fireEvent.change(searchInput, { target: { value: "John Doe" } });
+    await wait(200);
+    const idOfIncludedElement = screen.getByTestId(`table-row-${"1"}`);
+    const idOfExcludedElement = screen.getByTestId(`table-row-${"2"}`);
+
+    expect(idOfIncludedElement).toBeInTheDocument();
+    expect(idOfExcludedElement).not.toBeInTheDocument();
   });
 });
