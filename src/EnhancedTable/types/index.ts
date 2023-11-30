1eb493a7913ee759368ce82f24af02af308838df
saharso <saharso@gmail.com>
2023-11-30 07:05:26 +0200

    new type for selec option

--- src/EnhancedTable/types/index.ts
+++ src/EnhancedTable/types/index.ts
@@ -3,3 +3,4 @@ export type { default as RowUpdatePayload } from "./RowUpdatePayload";
 export type { default as CellEditPayload } from "./CellEditPayload";
 export type { default as GroupBy } from "./GroupBy";
 export type { default as Pojo } from "./Pojo";
+export type { default as SelectOption } from "./SelectOption";
